14298fa07a66c4a43faa2ad87c1b5cbb9dfc920a
yanggoo <qewrt1124@naver.com>
2023-07-13 12:07:03 +0900

    스크린샷 기능 추가, package.json 수정

--- hello.js
+++ hello.js
@@ -3,15 +3,25 @@ const json2csv = require("json2csv");
 const fs = require("fs");
 
 function main() {
-    let filename = "12b";
-    let url = "https://www.virustotal.com/gui/file/12bab34ccc85de745615b8fa63ae9ca1bbfb45b5e73b18b800096f827d024c49";
+    let filename = "106518";
+    let url = "https://www.virustotal.com/gui/file/a73485420f078fa279bb65b476838cb8307c0bbf48979a8a8fdcf06b2b314add";
+    let check= false;
+    // let check = true;
 
-    test(url, filename);
+    test(url, filename, check);
 }
 
-async function test(url, filename) {
-    const browser = await puppeteer.launch();
+async function test(url, filename, check) {
+    const browser = await puppeteer.launch({
+        args:["--windows-size=1920,1080"]
+    });
+
     const page = await browser.newPage();
+    await page.setViewport({
+        width: 1920,
+        height: 1080
+    });
+
     await page.goto(url + "/detection");
 
     let data = await page.evaluate(() => {
@@ -30,30 +40,45 @@ async function test(url, filename) {
         let flex_list = [];
 
         for(let i = 0; i < flex.length; i++) {
-            let result = document.querySelector("#view-container").querySelector("file-view").shadowRoot.querySelector("#report").querySelector("vt-ui-file-card").shadowRoot.querySelector(".card").querySelector(".card-body").querySelector(".vstack").querySelector(".flex-wrap").querySelectorAll("a")[i].innerText.trim();
+            let result = flex[i].innerText.trim();
+
             flex_list.push(result);
         }
 
-        // Popular
+        const label_list = document.querySelector("#view-container > file-view").shadowRoot.querySelector("#report > .tab-slot > .popular-threat-name").querySelectorAll(".col");
+
         const Popular = [];
-        Popular.push(document.querySelector("#view-container > file-view").shadowRoot.querySelector("#report > span > div > div:nth-child(1) > a").innerText);
-
-        // Threat
-        const Threat = document.querySelector("#view-container > file-view").shadowRoot.querySelector("#report > span > div > div:nth-child(2) > div").querySelectorAll("a");
-
         let Threat_list = [];
-        for(let i = 0; i < Threat.length; i++) {
-            let result = document.querySelector("#view-container > file-view").shadowRoot.querySelector("#report > span > div > div:nth-child(2) > div").querySelectorAll("a")[i].innerText.trim();
-            Threat_list.push(result);
-        }
-
-        // Family
-        const Family = document.querySelector("#view-container > file-view").shadowRoot.querySelector("#report > span > div > div:nth-child(3) > div").querySelectorAll("a");
-
         let Family_list = [];
-        for(let i = 0; i < Family.length; i++) {
-            let result = document.querySelector("#view-container > file-view").shadowRoot.querySelector("#report > span > div > div:nth-child(3) > div").querySelectorAll("a")[i].innerText.trim();
-            Family_list.push(result);
+
+        for(let i = 0; i < label_list.length; i++) {
+
+            let title = label_list[i].querySelector(".fw-bold").innerText.trim();
+            // Popular
+            if(title === "Popular threat label") {
+                const po_list = label_list[i].querySelectorAll("a");
+                for(let j = 0; j < po_list.length; j++) {
+                    Popular.push(po_list[j].innerText.trim());
+                }
+            }
+
+            // Threat
+            if(title === "Threat categories") {
+                const Threat = label_list[i].querySelector(".tags").querySelectorAll("a");
+
+                for(let j = 0; j < Threat.length; j++) {
+                    Threat_list.push(Threat[j].innerText.trim());
+                }
+            }
+
+            // Family
+            if(title === "Family labels") {
+                const Family = label_list[i].querySelector(".tags").querySelectorAll("a");
+
+                for(let j = 0; j < Family.length; j++) {
+                    Family_list.push(Family[j].innerText.trim());
+                }
+            }
         }
 
         let microsoft;
@@ -97,49 +122,67 @@ async function test(url, filename) {
         }
     });
 
-    await browser.close();
+    let data2;
 
     // behavior
-    const browser2 = await puppeteer.launch();
-    const page2 = await browser2.newPage();
-    await page2.goto(url + "/behavior");
-    let data2 = await page2.evaluate(() => {
+    if(check) {
+        const browser2 = await puppeteer.launch();
+        const page2 = await browser2.newPage();
+        await page2.setRequestInterception(true);
+        page2.on('request', (req) => {
+            if (
+                req.resourceType() === 'image' ||
+                req.resourceType() === 'font' ||
+                req.resourceType() === 'stylesheet'
+            ) {
+                // 만약 요청 타입이 '이미지' or 'CSS' or '폰트' 라면
+                req.abort(); // 거부
+            } else {
+                // 이미지가 아니라면
+                req.continue(); // 수락
+            }
+        });
 
-        // behavior not found
-        const status = document.querySelector("#view-container > file-view").shadowRoot.querySelector("#behaviourtab").shadowRoot.querySelector("div:nth-child(4) > div > div > div > div:nth-child(2) > div:nth-child(2)").querySelector("span").innerText;
+        await page2.goto(url + "/behavior");
 
-        let Mitre_list = [];
-        let detection_list;
-        let description_list;
+        data2 = await page2.evaluate(() => {
+            // behavior not found
+            const status = document.querySelector("#view-container > file-view").shadowRoot.querySelector("#behaviourtab").shadowRoot.querySelector("div:nth-child(4) > div > div > div > div:nth-child(2) > div:nth-child(2)").querySelector("span").innerText;
 
-        if(status !== "NOT FOUND") {
-            const h5_list = document.querySelector("#view-container > file-view").shadowRoot.querySelector("#behaviourtab").shadowRoot.querySelector("#mitre-tree").shadowRoot.querySelector("vt-ui-expandable > span:nth-child(2) > div").querySelectorAll("h5");
+            let Mitre_list = [];
+            let detection_list;
+            let description_list;
 
-            const table_list = document.querySelector("#view-container > file-view").shadowRoot.querySelector("#behaviourtab").shadowRoot.querySelector("#mitre-tree").shadowRoot.querySelector("vt-ui-expandable > span:nth-child(2) > div").querySelectorAll("table");
+            console.log(status);
 
-            for(let i = 0; i < h5_list.length; i++) {
-                Mitre_list.push(h5_list[i].querySelector(".hstack").querySelector("a").innerText);
-                Mitre_list.push(h5_list[i].querySelector(".hstack").querySelector("span").innerText);
-                detection_list = table_list[i].querySelectorAll("tr");
-                for(let j = 0; j < detection_list.length; j++) {
-                    description_list = detection_list[j].querySelector("span").querySelectorAll("div");
-                    Mitre_list.push(description_list[0].querySelector("a").innerText);
-                    Mitre_list.push(description_list[0].querySelector("span").innerText);
-                    aa_list = detection_list[j].querySelectorAll(".mitre-signature");
-                    for(let k = 0; k < aa_list.length; k++) {
-                        Mitre_list.push(aa_list[k].querySelector(".mb-0").innerText);
+            if(status !== "NOT FOUND") {
+                const h5_list = document.querySelector("#view-container > file-view").shadowRoot.querySelector("#behaviourtab").shadowRoot.querySelector("#mitre-tree").shadowRoot.querySelector("vt-ui-expandable > span:nth-child(2) > div").querySelectorAll("h5");
+
+                const table_list = document.querySelector("#view-container > file-view").shadowRoot.querySelector("#behaviourtab").shadowRoot.querySelector("#mitre-tree").shadowRoot.querySelector("vt-ui-expandable > span:nth-child(2) > div").querySelectorAll("table");
+
+                for(let i = 0; i < h5_list.length; i++) {
+                    Mitre_list.push(h5_list[i].querySelector(".hstack").querySelector("a").innerText);
+                    Mitre_list.push(h5_list[i].querySelector(".hstack").querySelector("span").innerText);
+                    detection_list = table_list[i].querySelectorAll("tr");
+                    for(let j = 0; j < detection_list.length; j++) {
+                        description_list = detection_list[j].querySelector("span").querySelectorAll("div");
+                        Mitre_list.push(description_list[0].querySelector("a").innerText);
+                        Mitre_list.push(description_list[0].querySelector("span").innerText);
+                        aa_list = detection_list[j].querySelectorAll(".mitre-signature");
+                        for(let k = 0; k < aa_list.length; k++) {
+                            Mitre_list.push(aa_list[k].querySelector(".mb-0").innerText);
+                        }
                     }
-                    // Mitre_list.push(detection_list[j].querySelector(".mb-0").innerText);
                 }
             }
-        }
 
-        return {
-            mitre_list : Mitre_list
-        }
-    });
+            return {
+                mitre_list : Mitre_list
+            }
+        });
 
-    await browser2.close();
+        await browser2.close();
+    }
 
     // scv 파일 형식
     let end = {
@@ -165,7 +208,11 @@ async function test(url, filename) {
     arr_length.push(data.Popular.length);
     arr_length.push(data.Threat_list.length);
     arr_length.push(data.Family_list.length);
-    arr_length.push(data2.mitre_list.length);
+
+    // mitre 자동 입력
+    if(check) {
+        arr_length.push(data2.mitre_list.length);
+    }
 
     let max_length = Math.max.apply(null, arr_length);
 
@@ -211,14 +258,18 @@ async function test(url, filename) {
     // tencent 집어 넣기
     end_list[0].tencent = data.Tencent;
 
-    // mitre 집어 넣기
-    for(let i = 0; i < data2.mitre_list.length; i++) {
-        end_list[i].mitre = data2.mitre_list[i];
+    // mitre 자동 집어 넣기
+    if(check) {
+        for(let i = 0; i < data2.mitre_list.length; i++) {
+            end_list[i].mitre = data2.mitre_list[i].trim();
+        }
     }
 
     // csv 파일 쓰기
     const csv_data = json2csv.parse(end_list);
     const directory = "C:\\Users\\qewrt\\alba\\secure\\";
+
+    // console.log(csv_data);
 
     fs.writeFileSync(directory + filename + ".csv", csv_data, (err) => {
         if(err) {
@@ -228,7 +279,20 @@ async function test(url, filename) {
         }
     });
 
+    let sc_path = "C:\\Users\\qewrt\\OneDrive\\사진\\스크린샷\\";
+
+    await  page.screenshot({
+        path: sc_path + filename + ".jpg",
+        fullPage: false,
+        clip: {
+            x: 0,
+            y: 0,
+            width: 1920,
+            height: 1033
+        }
+    });
     console.log(filename)
+    await browser.close();
 }
 
 main();
